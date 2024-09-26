b551bb51f324603ed0ed03d25c58809611ba5bc9
ryslee <ryslee@umich.edu>
2024-09-26 14:46:36 -0400

    part 3

--- part 3/__jac_gen__/server.py
+++ part 3/__jac_gen__/server.py
@@ -16,7 +16,7 @@ else:
     RagEngine, = __jac_import__(target='rag', base_path=__file__, lng='jac', absorb=False, mdl_alias=None, items={'RagEngine': None})
 rag_engine: RagEngine = RagEngine()
 
-@_Jac.make_walker(on_entry=[_Jac.DSFunc('init_session', _Jac.RootType), _Jac.DSFunc('chat', Session)], on_exit=[])
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('init_session', _Jac.RootType)], on_exit=[])
 @__jac_dataclass__(eq=False)
 class interact(_Jac.Walker):
     message: str
@@ -30,13 +30,6 @@ class interact(_Jac.Walker):
             print('Session Node Created')
             if _Jac.visit_node(self, session_node):
                 pass
-
-    def chat(self, _jac_here_: Session) -> None:
-        _jac_here_.chat_history.append({'role': 'user', 'content': self.message})
-        data = rag_engine.get_from_chroma(query=self.message)
-        response = _jac_here_.llm_chat(message=self.message, chat_history=_jac_here_.chat_history, agent_role='You are a conversation agent designed to help users with their queries based on the documents provided', context=data)
-        _jac_here_.chat_history.append({'role': 'assistant', 'content': response})
-        _Jac.report({'response': response})
 
 @_Jac.make_node(on_entry=[], on_exit=[])
 @__jac_dataclass__(eq=False)
@@ -58,6 +51,7 @@ class infer(_Jac.Walker):
             router_node = _Jac.connect(left=_jac_here_, right=Router(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
             _Jac.connect(left=router_node, right=RagChat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
             _Jac.connect(left=router_node, right=QAChat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
+            _Jac.connect(left=router_node, right=TriviaChat(), edge_spec=_Jac.build_edge(is_undirected=False, conn_type=None, conn_assign=None))
             if _Jac.visit_node(self, router_node):
                 pass
 
@@ -85,6 +79,7 @@ class Session(_Jac.Node):
 class ChatType(__jac_Enum__):
     RAG = 'RAG'
     QA = 'user_qa'
+    Trivia = 'give trivia'
 
 @_Jac.make_node(on_entry=[], on_exit=[])
 @__jac_dataclass__(eq=False)
@@ -113,3 +108,14 @@ class QAChat(Chat, _Jac.Node):
         def respond_with_llm(message: str, chat_history: list[dict], agent_role: str) -> str:
             return _Jac.with_llm(file_loc=__file__, model=llm, model_params={}, scope='server(Module).QAChat(node).respond(Ability).respond_with_llm(Ability)', incl_info=[], excl_info=[], inputs=[('current message', str, 'message', message), ('chat history', list[dict], 'chat_history', chat_history), ('role of the agent responding', str, 'agent_role', agent_role)], outputs=('response', 'str'), action='Respond to message using chat_history as context and agent_role as the goal of the agent', _globals=globals(), _locals=locals())
         _jac_here_.response = respond_with_llm(_jac_here_.message, _jac_here_.chat_history, agent_role='You are a conversation agent designed to help users with their queries')
+
+@_Jac.make_node(on_entry=[_Jac.DSFunc('respond', infer)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class TriviaChat(Chat, _Jac.Node):
+    chat_type: ChatType = _Jac.has_instance_default(gen_func=lambda: ChatType.Trivia)
+
+    def respond(self, _jac_here_: infer) -> None:
+
+        def respond_with_llm(message: str, chat_history: list[dict], agent_role: str) -> str:
+            return _Jac.with_llm(file_loc=__file__, model=llm, model_params={}, scope='server(Module).TriviaChat(node).respond(Ability).respond_with_llm(Ability)', incl_info=[], excl_info=[], inputs=[('current message', str, 'message', message), ('chat history', list[dict], 'chat_history', chat_history), ('role of the agent responding', str, 'agent_role', agent_role)], outputs=('response', 'str'), action='Respond to message using chat_history as context and agent_role as the goal of the agent', _globals=globals(), _locals=locals())
+        _jac_here_.response = respond_with_llm(_jac_here_.message, _jac_here_.chat_history, agent_role='You are a trivia expert ready to answer questions from various categories.')
